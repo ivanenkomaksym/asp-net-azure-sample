583f5e278daa448eb339a813e574a75502dddb6f
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-05-06 17:28:21 +0300

    Add standard razor pages. Add JS support for MSAL authentication. Call WeatherForecast API.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_net_azure_sample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ wwwroot/js/clientConfig.js
@@ -0,0 +1,11 @@
+﻿const clientConfig = {
+    // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+    clientId: [Enter the UI client id here],
+    // Full directory URL, in the form of https://login.microsoftonline.com/<tenant-id>
+    authority: "https://login.microsoftonline.com/organizations",
+    // Full redirect URL, in form of http://localhost:3000
+    redirectUri: "https://localhost:44321",
+    // Scope
+    scope: [Enter the server scope here],
+    weatherForecastURI: "https://localhost:44321/WeatherForecast"
+};
