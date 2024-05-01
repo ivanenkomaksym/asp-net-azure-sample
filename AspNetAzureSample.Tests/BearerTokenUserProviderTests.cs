6c320ecafad782569df1d7616f819c313d6695bb
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-05-01 11:11:42 +0300

    Update unit tests. Cover getting username from the token with no claims and with email claim. Extract common GenerateToken method.

--- AspNetAzureSample.Tests/BearerTokenUserProviderTests.cs
+++ AspNetAzureSample.Tests/BearerTokenUserProviderTests.cs
@@ -12,21 +12,80 @@ namespace AspNetAzureSample.Tests
     public class BearerTokenUserProviderTests
     {
         [Fact]
+        public void GetUserName_WithBearerTokenNoClaims_ReturnsEnvironmentUserName()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<DefaultUserProvider>>();
+            var provider = new BearerTokenUserProvider(logger);
+            var expectedUserName = Environment.UserName;
+
+            var jwtToken = GenerateToken(Array.Empty<Claim>());
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = "Bearer " + jwtToken;
+
+            // Act
+            var userName = provider.GetUserName(httpContext);
+
+            // Assert
+            Assert.Equal(expectedUserName, userName);
+        }
+
+        [Fact]
         public void GetUserName_WithValidBearerToken_ReturnsNameFromToken()
         {
             // Arrange
             var logger = Substitute.For<ILogger<DefaultUserProvider>>();
             var provider = new BearerTokenUserProvider(logger);
+            var expectedUserName = "JohnDoe";
 
+            var claims = new List<Claim>
+            {
+                new Claim("unique_name", expectedUserName),
+                new Claim("ipaddr", "192.168.1.1")
+            };
+            var jwtToken = GenerateToken(claims);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = "Bearer " + jwtToken;
+
+            // Act
+            var userName = provider.GetUserName(httpContext);
+
+            // Assert
+            Assert.Equal(expectedUserName, userName);
+        }
+
+        [Fact]
+        public void GetUserName_WithEmailClaimBearerToken_ReturnsEmailFromToken()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<DefaultUserProvider>>();
+            var provider = new BearerTokenUserProvider(logger);
+            var expectedEmail = "johndoe@gmail.com";
+
+            var claims = new List<Claim>
+            {
+                new Claim("email", expectedEmail),
+            };
+            var jwtToken = GenerateToken(claims);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = "Bearer " + jwtToken;
+
+            // Act
+            var userName = provider.GetUserName(httpContext);
+
+            // Assert
+            Assert.Equal(expectedEmail, userName);
+        }
+
+        private string GenerateToken(IEnumerable<Claim> claims)
+        {
             var keyBytes = Encoding.UTF8.GetBytes(Secret);
             var securityKey = new SymmetricSecurityKey(keyBytes);
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
             var now = DateTime.UtcNow;
-            var claims = new List<Claim>
-            {
-                new Claim("unique_name", "JohnDoe"),
-                new Claim("ipaddr", "192.168.1.1")
-            };
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
@@ -37,15 +96,7 @@ namespace AspNetAzureSample.Tests
             var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
             var token = jwtSecurityTokenHandler.CreateToken(tokenDescriptor);
             var jwtToken = jwtSecurityTokenHandler.WriteToken(token);
-
-            var httpContext = new DefaultHttpContext();
-            httpContext.Request.Headers["Authorization"] = "Bearer " + jwtToken;
-
-            // Act
-            var userName = provider.GetUserName(httpContext);
-
-            // Assert
-            Assert.Equal("JohnDoe", userName);
+            return jwtToken;
         }
 
         private const string Secret = "4777E6A4592BF1C9D5E62BFB41A77";
