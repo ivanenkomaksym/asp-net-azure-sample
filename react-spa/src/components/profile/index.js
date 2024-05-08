4b1991946fea4d1b929a8d7ff0e1f997c9e77c31
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-05-08 10:44:34 +0300

    Implement Profile page.

--- react-spa/src/components/profile/index.js
+++ react-spa/src/components/profile/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 
 function Profile() {
+    // Retrieve user information from local storage
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+
     return (
         <div>
+            {/* Check if user information exists */}
+            {userInfo && (
+                <div>
+                    <h2>User Profile</h2>
+                    <p>First Name: {userInfo.firstName}</p>
+                    <p>Last Name: {userInfo.lastName}</p>
+                    <p>Email: {userInfo.email}</p>
+                    <img src={userInfo.picture} alt="User Profile" />
+                </div>
+            )}
+            {/* Display a message if user information is not available */}
+            {!userInfo && (
+                <div>
+                    <p>No user information available.</p>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Profile;
