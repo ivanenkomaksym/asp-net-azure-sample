364990caaadc6980c88fbaa8592ca7a82c18765e
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-05-16 17:29:02 +0300

    Use table to visualize weather data. Use css styles.

--- react-spa/src/components/home/index.js
+++ react-spa/src/components/home/index.js
@@ -5,9 +5,11 @@ import * as api from "../../api/index"
 
 import { PublicClientApplication, InteractionRequiredAuthError } from '@azure/msal-browser';
 import { weatherForecastTokenRequest, msalConfig } from '../../authConfig';
+import { MICROSOFT_IP, GOOGLE_IP } from '../../redux/const/actionsTypes';
 
 function Home() {
     const [userData, setUserData] = useState(null);
+    const [errorMessage, setError] = useState(null);
     const [weatherData, setWeatherData] = useState(null);
     const location = useLocation();
 
@@ -56,20 +58,32 @@ function Home() {
                         });
                 } else {
                     console.warn(error);
+                    setError(error.message);
                 }
             });
     }
 
     async function weatherForecast() {
-        return await getTokenPopup(weatherForecastTokenRequest)
-            .then(response => {
-                api.weatherForecast(response.accessToken).then((result) => {
-                    console.log("data: ", JSON.stringify(result, null, 2));
-                    setWeatherData(result);
+        if (userData.identityProvider == MICROSOFT_IP) {
+            return await getTokenPopup(weatherForecastTokenRequest)
+                .then(response => {
+                    api.weatherForecast(response.accessToken).then((result) => {
+                        console.log("data: ", JSON.stringify(result, null, 2));
+                        setWeatherData(result);
+                    });
+                }).catch(error => {
+                    console.error(error);
+                    setError(error.message);
                 });
+        } else if (userData.identityProvider == GOOGLE_IP) {
+            api.weatherForecast(userData.token).then((result) => {
+                console.log("data: ", JSON.stringify(result, null, 2));
+                setWeatherData(result);
             }).catch(error => {
                 console.error(error);
+                setError(error.message);
             });
+        }
     }
 
     async function handleGetWeather() {
@@ -77,11 +91,12 @@ function Home() {
             weatherForecast();
         } catch (error) {
             console.error("Error fetching weather data:", error);
+            setError(error.message);
         }
     };
 
     return (
-        <div>
+        <div className="container">
             {userData && (
                 <button onClick={handleGetWeather} className="getWeatherButton">
                     Get Weather
@@ -89,14 +104,32 @@ function Home() {
             )}
             {weatherData && (
                 <div className="weatherData">
-                    <h2>Weather Forecast</h2>
-                    {weatherData.map((forecast, index) => (
-                        <div key={index} className="forecastItem">
-                            <p>Date: {forecast.date}</p>
-                            <p>Temperature: {forecast.temperatureC}°C ({forecast.temperatureF}°F)</p>
-                            <p>Summary: {forecast.summary}</p>
-                        </div>
-                    ))}
+                    <h2 className="weatherTitle">Weather Forecast</h2>
+                    <table className="forecastTable">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Temperature (°C)</th>
+                                <th>Temperature (°F)</th>
+                                <th>Summary</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {weatherData.map((forecast, index) => (
+                                <tr key={index}>
+                                    <td>{forecast.date}</td>
+                                    <td>{forecast.temperatureC}</td>
+                                    <td>{forecast.temperatureF}</td>
+                                    <td>{forecast.summary}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+            {errorMessage && (
+                <div className="errorContainer">
+                    <p className="errorMessage">{errorMessage}</p>
                 </div>
             )}
         </div>
