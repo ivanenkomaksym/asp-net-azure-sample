da0100a05b49ff86bddd666937079cfb9ad872f1
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-08-01 14:57:39 +0300

    Make selecting an organization optional on LoginOrg page.

--- react-spa/src/components/loginorg/index.js
+++ react-spa/src/components/loginorg/index.js
@@ -5,8 +5,11 @@ import { loginOrgAuthorizeUrl } from "../../authConfig";
 import User from "../../models/user"
 import { ORGANIZATION } from "../../redux/const/actionsTypes";
 import { saveUserToLocalStorage } from "../../redux/reducers/auth";
+import isSelectOrganizationEnabled from "../../useFeatureFlags";
 
 function LoginOrg() {
+    const isSelectOrganization = isSelectOrganizationEnabled();
+
     const [domains, setDomains] = useState([]);
 
     const [email, setEmail] = useState("");
@@ -26,7 +29,7 @@ function LoginOrg() {
     };
 
     const [validationErrors, setValidationErrors] = useState(""); // State to store validation errors
-    const errorHandler = (err) => {        
+    const errorHandler = (err) => {
         console.log("error response: ", JSON.stringify(err, null, 2));
         if (err) {
             setValidationErrors("This email address is not supported.");
@@ -63,54 +66,86 @@ function LoginOrg() {
                                   /*organization    */selectedOrganization);
             saveUserToLocalStorage(user);
             console.log(`email=${email}&organization=${selectedOrganization}`);
-            window.location.assign(`${loginOrgAuthorizeUrl}?email=${email}&organization=${selectedOrganization}`);
+
+            const loginUri = `${loginOrgAuthorizeUrl}?email=${email}`;
+            if (isSelectOrganization)
+                loginUri += `&organization=${selectedOrganization}`;
+            window.location.assign(loginUri);
         }
     });
 
     return (
-        <div className={LoginorgStyles.loginContainer}>
-            <div className={LoginorgStyles.loginContainerv2}>
-                <h1>Welcome back</h1>
-
-                {emailSet ? (
-                    <>
-                    <div className={LoginorgStyles.emailDisplayContainer}>
-                        <label>Email address</label>
-                        <br></br>
-                        <div className={LoginorgStyles.emailAddress}>
-                            {email}
+        <>
+            {!isSelectOrganization ? (
+                <>
+                    <div className={LoginorgStyles.loginContainer}>
+                        <div className={LoginorgStyles.loginContainerv2}>
+                            <h1>Welcome back</h1>
+                            <div className={LoginorgStyles.inputContainer}>
+                                <label>EMAIL</label>
+                                <input
+                                    onChange={e => setEmail(e.target.value)}
+                                    placeholder="enter your email"
+                                    type="email"
+                                />
+                            </div>
+                            {validationErrors && (
+                                <span className={LoginorgStyles.errorMsg}>{validationErrors}</span>
+                            )}
+                            <div>
+                                <button onClick={() => setClicked(true)} className={LoginorgStyles.loginBTN}>LOGIN</button>
+                            </div>
                         </div>
-                        <button onClick={handleChangeEmail} className={LoginorgStyles.changeEmailBTN}>Change</button>
                     </div>
-                    <div className="dropdownContainer">
-                        <label>I'm with</label>
-                        {domains != null && (
-                            <select value={selectedOrganization} onChange={e => setSelectedOrganization(e.target.value)} className="selectDropdown">
-                                {domains.map(domain => (
-                                    <option key={domain.id} value={domain.id}>{domain.description}</option>
-                                ))}
-                            </select>
-                        )}
-                    </div><button onClick={() => setClicked(true)} className={LoginorgStyles.loginBTN}>LOGIN</button>
-                    </>
-                ) : (
-                    <>
-                    <div className={LoginorgStyles.inputContainer}>
-                        <label>EMAIL</label>
-                        <input
-                            onChange={e => setEmail(e.target.value)}
-                            placeholder="enter your email"
-                            type="email"
-                        />
+                </>
+            ) : (
+                <>
+                    <div className={LoginorgStyles.loginContainer}>
+                        <div className={LoginorgStyles.loginContainerv2}>
+                            <h1>Welcome back</h1>
+
+                            {emailSet ? (
+                                <>
+                                    <div className={LoginorgStyles.emailDisplayContainer}>
+                                        <label>Email address</label>
+                                        <br></br>
+                                        <div className={LoginorgStyles.emailAddress}>
+                                            {email}
+                                        </div>
+                                        <button onClick={handleChangeEmail} className={LoginorgStyles.changeEmailBTN}>Change</button>
+                                    </div>
+                                    <div className="dropdownContainer">
+                                        <label>I'm with</label>
+                                        {domains != null && (
+                                            <select value={selectedOrganization} onChange={e => setSelectedOrganization(e.target.value)} className="selectDropdown">
+                                                {domains.map(domain => (
+                                                    <option key={domain.id} value={domain.id}>{domain.description}</option>
+                                                ))}
+                                            </select>
+                                        )}
+                                    </div><button onClick={() => setClicked(true)} className={LoginorgStyles.loginBTN}>LOGIN</button>
+                                </>
+                            ) : (
+                                <>
+                                    <div className={LoginorgStyles.inputContainer}>
+                                        <label>EMAIL</label>
+                                        <input
+                                            onChange={e => setEmail(e.target.value)}
+                                            placeholder="enter your email"
+                                            type="email"
+                                        />
+                                    </div>
+                                    {validationErrors != null && (
+                                        <span className={LoginorgStyles.errorMsg}>{validationErrors}</span>
+                                    )}
+                                    <button onClick={handleEmailSet} className={LoginorgStyles.loginBTN}>Continue</button>
+                                </>
+                            )}
+                        </div>
                     </div>
-                    {validationErrors != null && (
-                        <span className={LoginorgStyles.errorMsg}>{validationErrors}</span>
-                    )}
-                    <button onClick={handleEmailSet} className={LoginorgStyles.loginBTN}>Continue</button>
-                    </>
-                )}
-            </div>
-        </div>
+                </>
+            )}
+        </>
     );
 }
 
